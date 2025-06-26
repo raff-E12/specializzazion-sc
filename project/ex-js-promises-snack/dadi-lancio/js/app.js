faeea89cd5b6005095be30e538109b24d2b020fc
Raff_12 <raffelecoppola381@gmail.com>
2025-06-26 13:15:48 +0200

    feat: Modifica delle funzione..

--- project/ex-js-promises-snack/dadi-lancio/js/app.js
+++ project/ex-js-promises-snack/dadi-lancio/js/app.js
@@ -2,16 +2,24 @@
 // Snacks - Le Promises
 // Creare con le Promises il Gioco del Lancio dei Dadi.
 
-function lanciaDado() {
+function CrealanciaDado() {
+    let num_prev = null;
+
+    return function(){
     const interruption = Math.random() < 0.2;
     console.log("Processo in esecuzione...")
     const process = new Promise((resolve, reject) => {
        setTimeout(() => {
           if (interruption) {
+             num_prev = null;
              reject("Il Dado è Incastrato!!, Riprova.");
           } else {
              const num = Math.floor(Math.random() * 6);
              if (num > 0) {
+                if (num === num_prev) {
+                    console.log("Incredibile!!");
+                }
+                num_prev = num;
                 resolve("Hai Vinto");
              } else {
                 reject("Hai Perso!!");
@@ -21,6 +29,14 @@ function lanciaDado() {
     })
 
     return process
+    }
 }
 
-lanciaDado().then(win => console.log(win)).catch(lose => console.error(lose))
+const refresh_fun = CrealanciaDado();
+
+refresh_fun().then(win => {
+     console.log(win);
+    refresh_fun().then(win => {console.log(win)
+    }).catch(lose => console.error(lose));
+    
+}).catch(lose => console.error(lose));
