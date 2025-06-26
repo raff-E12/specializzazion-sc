d54034bfd15e9e8f08262a2c551a96d2412325ca
Raff_12 <raffelecoppola381@gmail.com>
2025-06-26 12:08:38 +0200

    feat: Risoluzione del esercizio..

--- project/ex-js-promises-snack/js/app.js
+++ project/ex-js-promises-snack/js/app.js
@@ -1,11 +1,12 @@
 
 // Snacks - Le Promises
+// Scopo Creare una Funzione che ospita una Promises.
 
 function getPostTitle(id) {
   const fetch_url = `https://dummyjson.com/posts/${id}`;
   const fetching_data = new Promise((resolve, reject) => {
        setTimeout(() => {
-         fetch(fetch_url).then( response => response.json())
+        fetch(fetch_url).then(response => response.json())
         .then(data => resolve(data))
         .catch(error => reject(error))
        }, 1900);
@@ -27,7 +28,8 @@ function getPost(id) {
   return fetching_data
 }
 
-let id_users = null;
-let obj_fetch = {};
-getPostTitle(1).then(result => console.log(result)).catch(error => console.error(error));
-getPost(1).then(result => console.log(result)).catch(error => console.error(error));
+const await_fetch = getPostTitle(4).then(result => {
+    let id = result.id;
+    getPost(id).then(result => console.log(result)).
+    catch(error => console.error(error)); 
+}).catch(error => console.error(error));
