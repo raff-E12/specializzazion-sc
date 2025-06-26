ca9edddc1aeefb95ba8cebb55ee8ef490836aacf
Raff_12 <raffelecoppola381@gmail.com>
2025-06-26 20:23:12 +0200

    feat: Modifica della funzione..

--- project/ex-js-async-chef/js/app.js
+++ project/ex-js-async-chef/js/app.js
@@ -29,41 +29,35 @@ async function getChefBirthday(id) {
     const recipes_users = `https://dummyjson.com/recipes`;
     const users_url = `https://dummyjson.com/users`;
 
-    let chef = null;
-    let recipes = null;
     let userId = 0;
-    let combination = {};
+    let bithday = null;
 
-    if (recipes === null && chef === null) {
+        let recipes = null;
         try {
             recipes = await HandleFetchingData(`${recipes_users}/${id}`);
             userId = recipes.userId;
+            if (userId === undefined) return console.warn("ID Utente Non Trovato, Riprova!!");
+        } catch (error) {
+            throw new Error("Impossibile Recuperare le informazioni ottenute.");
+        }
+        
+        if (recipes.message) return console.error(recipes.message);
 
-            if (userId !== undefined) {
-                chef = await HandleFetchingData(`${users_url}/${userId}`);
-            } else {
-                console.warn("ID Vuoto, Riprova!!");
-            }
-
-            combination = {...recipes, chef: {...chef}};
+        let chef = null;
+        try {
+            chef = await HandleFetchingData(`${users_url}/${userId}`);
+            bithday = dayjs(chef.birthDate).format('DD/MM/YYYY');
         } catch (error) {
             throw new Error("Impossibile Recuperare le informazioni ottenute.");
         }
 
-        if (recipes.message) return console.error(recipes.message);
-        if (chef.message) return console.error(chef.message);
-    }
-
-    return combination
+        return console.log("Data di nascita dello chef:", bithday);;
 }
 
 const api_response = (async () => {
     try {
-        const list = await getChefBirthday(12);
-        const username = list.chef.username;
-        const bithday = dayjs(list.chef.birthDate).format('DD/MM/YYYY');
-        return console.log("Chef:", username, "Nascita:", bithday);
-    } catch (error) {
+        const bithday = await getChefBirthday(2);
+    } catch (error) { 
         throw new Error(error.message);
     }
 })();
