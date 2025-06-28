d6210277af07bd7de77359db996ed9b67bec3edf
Raff_12 <raffelecoppola381@gmail.com>
2025-06-28 12:19:06 +0200

    feat: Modifica dello script..

--- project/ex-js-promises-snack/dadi-lancio/js/tutorial.js
+++ project/ex-js-promises-snack/dadi-lancio/js/tutorial.js
@@ -1,7 +1,14 @@
 
     const dice = document.getElementById('dice');
+    const btn_roll = document.getElementById('btn-roll');
 
-    const faces = {
+    function LancioDadi(){
+      const interruption = Math.random() < 0.2;
+      const num = Math.floor(Math.random() * 6) + 1;
+      let index = 1;
+      dice.innerHTML = "";
+
+      const faces = {
       1: [5],
       2: [1, 9],
       3: [1, 5, 9],
@@ -10,23 +17,18 @@
       6: [1, 3, 4, 6, 7, 9]
     };
 
-    function rollDice() {
-      const roll = Math.floor(Math.random() * 6) + 1;
-      drawFace(roll);
-    }
 
-    function drawFace(number) {
-      dice.innerHTML = ''; // svuota
-      for (let i = 1; i <= 9; i++) {
-        const dot = document.createElement('div');
-        dot.classList.add('cell');
-        if (faces[number].includes(i)) {
-          dot.classList.add('dot');
-        }
-        dice.appendChild(dot);
+    while (index < 9) {
+      const dot = document.createElement('div');
+      dot.classList.add('cell');
+
+      if (faces[num].includes(index)) {
+        dot.classList.add('dot');
       }
+
+      dice.appendChild(dot);
+      index++;
+    }
     }
 
-    // Disegna il primo dado iniziale
-    // onclick="rollDice()" evento
-    drawFace(1);
+    btn_roll.addEventListener("click", LancioDadi)
