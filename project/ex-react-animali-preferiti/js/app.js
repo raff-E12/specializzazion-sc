f2646ee96bb868efa0fe8533d27ff7f365de7856
Raff_12 <raffelecoppola381@gmail.com>
2025-07-01 15:20:14 +0200

    fix: Modifica e adattamento..

--- project/ex-react-animali-preferiti/js/app.js
+++ project/ex-react-animali-preferiti/js/app.js
@@ -12,29 +12,21 @@ function Modal({
       title, 
       content, 
       show = false, 
-      onClose = () => {}
+      onClose = () => {},
+      value,
+      setValue,
+      onConfirm = () => {}
   }){
-    const [isValue, setValue] = useState("");
-
-    function AddAnimals() {
-       if(isValue !== ""){
-        const list = document.createElement("li");
-        const ParentElement = document.getElementById("list-animals");
-        list.textContent = isValue;
-        ParentElement.appendChild(list);
-       }
-    }
-
       return show && ReactDOM.createPortal(
           <div className="modal-container">
               <div className="modal">
                   <h2>{title}</h2>
                   <p>{content}</p>
                  <div className="inp-modal">
-                    <input id="input-add" value={isValue} onChange={(e) => setValue(e.target.value)} />
+                    <input id="input-add" value={value} onChange={(e) => setValue(e.target.value)} />
                         <div className="btns-modals">
                             <button onClick={onClose}>Annulla</button>
-                            <button onClick={AddAnimals}>Conferma</button>
+                            <button onClick={onConfirm}>Conferma</button>
                         </div>
                     </div>
                  </div>
@@ -47,6 +39,7 @@ function DatailsAnimals() {
     const [isRandom, SetRandom] = useState(["Elefante", "Coniglio", "Criceto", "Pulcino", "Orso"]);
     const [isAnimals, SetAnimals] = useState(["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"]);
     const [isShow, setShow] = useState(false);
+    const [isValue, setValue] = useState("");
 
     function AddAnimals() {
         const RandomAnimals = isRandom[Math.floor(Math.random() * isRandom.length)];
@@ -57,9 +50,22 @@ function DatailsAnimals() {
         ParentElement.appendChild(list);
     }
 
+    
+    function AddAnimalsInput() {
+       if(isValue !== ""){
+        const list = document.createElement("li");
+        const ParentElement = document.getElementById("list-animals");
+        list.textContent = isValue;
+        ParentElement.appendChild(list);
+        setShow(false);
+        setValue("");
+       }
+    }
+
     return(<>
     <button id="btn-add" onClick={AddAnimals}>Aggiungi Animale</button>
-    <Modal title={"Aggiungi un Animale a Piacimento"} content={"Aggiungi e stampa il messaggio"} show={isShow} onClose={() => setShow(false)}/>
+    <Modal title={"Aggiungi un Animale a Piacimento"} content={"Aggiungi e stampa il messaggio"} 
+    show={isShow} onClose={() => setShow(false)} value={isValue} setValue={setValue} onConfirm={AddAnimalsInput}/>
     <button onClick={() => setShow(true)}>Mostra PopUp</button>
     <details id="details-sc">
         <summary>Animali</summary>
