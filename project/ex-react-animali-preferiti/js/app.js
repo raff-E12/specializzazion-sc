da9af6c08ed141c17ed409c69e1751258706c314
Raff_12 <raffelecoppola381@gmail.com>
2025-07-01 14:42:02 +0200

    feat: Pubblicazione del progetto.

---
+++ project/ex-react-animali-preferiti/js/app.js
@@ -0,0 +1,75 @@
+
+// Itegrazione React con Javascript Vanilla
+const { createRoot } = ReactDOM;
+const { useState } = React;
+
+const root = document.querySelector(".lista-animali");
+
+const rootParent = createRoot(root);
+rootParent.render(<DatailsAnimals />)
+
+function Modal({
+      title, 
+      content, 
+      show = false, 
+      onClose = () => {}
+  }){
+    const [isValue, setValue] = useState("");
+
+    function AddAnimals() {
+       if(isValue !== ""){
+        const list = document.createElement("li");
+        const ParentElement = document.getElementById("list-animals");
+        list.textContent = isValue;
+        ParentElement.appendChild(list);
+       }
+    }
+
+      return show && ReactDOM.createPortal(
+          <div className="modal-container">
+              <div className="modal">
+                  <h2>{title}</h2>
+                  <p>{content}</p>
+                 <div className="inp-modal">
+                    <input id="input-add" value={isValue} onChange={(e) => setValue(e.target.value)}></input>
+                        <div className="btns-modals">
+                            <button onClick={onClose}>Annulla</button>
+                            <button onClick={AddAnimals}>Conferma</button>
+                        </div>
+                    </div>
+                 </div>
+          </div>,
+          document.body
+      )
+}
+
+function DatailsAnimals() {
+    const [isRandom, SetRandom] = useState(["Elefante", "Coniglio", "Criceto", "Pulcino", "Orso"]);
+    const [isAnimals, SetAnimals] = useState(["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"]);
+    const [isShow, setShow] = useState(false);
+
+    function AddAnimals() {
+        const RandomAnimals = isRandom[Math.floor(Math.random() * isRandom.length)];
+        console.log(RandomAnimals)
+        const list = document.createElement("li");
+        const ParentElement = document.getElementById("list-animals");
+        list.textContent = RandomAnimals;
+        ParentElement.appendChild(list);
+    }
+
+    return(<>
+    <button id="btn-add" onClick={AddAnimals}>Aggiungi Animale</button>
+    <Modal title={"Aggiungi un Animale a Piacimento"} content={"Aggiungi e stampa il messaggio"} show={isShow} onClose={() => setShow(false)}/>
+    <button onClick={() => setShow(true)}>Mostra PopUp</button>
+    <details id="details-sc">
+        <summary>Animali</summary>
+        <ul id="list-animals">
+            {isAnimals.map((element, index) => {
+                return(<>
+                <li key={index}>{element}</li>
+                </>)
+            })}
+        </ul>
+    </details>
+    </>)
+}
