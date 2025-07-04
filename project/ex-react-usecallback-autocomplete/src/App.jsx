590af0ee2ebd6c76df912cb4a9f9c8c098a09e02
Raff_12 <raffelecoppola381@gmail.com>
2025-07-04 12:34:48 +0200

    feat: Pubblicazione del esercizio..

---
+++ project/ex-react-usecallback-autocomplete/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from "axios"
+import { useCallback } from 'react';
+
+function DebounceFunction(callback, delay) {
+  let TimeSet = null;
+  return (value) => {
+    clearTimeout(TimeSet)
+    TimeSet = setTimeout(() => {callback(value)}, delay)
+  }
+}
+
+function App() {
+
+  const [isResults, setResults] = useState([]);
+  const [isSearch, setSearch] = useState("");
+
+  async function EndPointSearch(query) {
+      try {
+      const endpoint = `http://localhost:3333/products?search=${query}`;
+      const fetchings = await axios.get(endpoint);
+      const data = fetchings.data;
+      console.log(data)
+      setResults(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const CallbackEffect = useCallback(DebounceFunction(EndPointSearch, 500), []);
+
+  // Usare sempre una callback si utilizzano le clousere.
+  useEffect(() => { CallbackEffect(isSearch) }, [isSearch])
+
+  return (
+    <>
+    <section className="hero">
+      <h1>Benvenuto nel nostro marketplace</h1>
+      <p>Cerca prodotti, servizi o ispirazioni</p>
+
+      <div className="search-container">
+        <div className="search-box">
+          <input type="text" placeholder="Cerca qui..."  id='input-search' value={isSearch} onChange={(e) => setSearch(e.target.value)}/>
+          <div className="suggestions" id="suggestions-box">
+            {isResults.map((element, index) => {
+              if (isSearch !== "" && isSearch !== element.name) {
+                return(<>
+                <div key={index} onClick={() => setSearch(element.name)}><p>{element.name}</p></div>
+                </>)
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default App
