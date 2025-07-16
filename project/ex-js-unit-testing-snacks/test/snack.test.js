927d2c8ab8080c1e9d8b44b2927b0a2840275e44
Raff_12 <raffelecoppola381@gmail.com>
2025-07-16 22:57:12 +0200

    fix: Modifica del progetto...

--- project/ex-js-unit-testing-snacks/test/snack.test.js
+++ project/ex-js-unit-testing-snacks/test/snack.test.js
@@ -3,40 +3,46 @@
 
 const { getInitials, createSlug, average, isPalindrome, findPostById } = require("../app.js")
 
-// Snack-1
-// La funzione getInitials restituisce le iniziali di un nome completo.
+// Raggruppamento dei relativi Test.
 
-test('La funzione getInitials restituisce le iniziali di un nome completo.', () => { 
-    expect(getInitials("Mario Rossi")).toBe("M.R");
- })
+describe("Manipolazione Stringhe e Slug", () => {
+    // Snack-1
+    // La funzione getInitials restituisce le iniziali di un nome completo.
 
- // Snack-2
- // La funzione createSlug restituisce una stringa in lowercase.
- // La Funzione createSlug sostituzione gli spazi con "-" "."
- // La funzione createSlug lancia un errore se il titolo è vuoto o non valido.
+    test('La funzione getInitials restituisce le iniziali di un nome completo.', () => { 
+        expect(getInitials("Mario Rossi")).toBe("M.R");
+    })
 
- test("La funzione createSlug restituisce una stringa in lowercase.", () => {
-    expect(createSlug("Titolo Grande")).toBe("titolo-grande");
-    expect(createSlug("Titolo Grande")).toBe("titolo-grande");
-    expect(() => createSlug("")).toThrow("Il Titolo Non Valido");
- })
+    // Snack-2
+    // La funzione createSlug restituisce una stringa in lowercase.
+    // La Funzione createSlug sostituzione gli spazi con "-" "."
+    // La funzione createSlug lancia un errore se il titolo è vuoto o non valido.
 
- // Snack-3
- // La funzione "averange" calcola la media artimetica di un array di numeri.
+    test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+        expect(createSlug("Titolo Grande")).toBe("titolo-grande");
+        expect(createSlug("Titolo Grande")).toBe("titolo-grande");
+        expect(() => createSlug("")).toThrow("Il Titolo Non Valido");
+    })
 
- test('La funzione "averange" calcola la media artimetica di un array di numeri', () => { 
-     expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
-     expect(average([10, 20, 30, 40, 50])).toBe(30);
-  })
+    // Snack-4
+    // La funzione isPalindrome verifica se una stringa è un palindromo.
 
-  // Snack-4
-  // La funzione isPalindrome verifica se una stringa è un palindromo.
+    test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => { 
+        expect(isPalindrome("Anna")).toBeTruthy();
+        //    expect(isPalindrome("Mario")).toBeFalsy();
+        expect(isPalindrome("Osso")).toBeTruthy();
+    })
 
-  test('La funzione isPalindrome verifica se una stringa è un palindromo.', () => { 
-       expect(isPalindrome("Anna")).toBeTruthy();
-    //    expect(isPalindrome("Mario")).toBeFalsy();
-       expect(isPalindrome("Osso")).toBeTruthy();
-   })
+})
+
+describe("Manipolazione Liste e Oggetti", () => {
+    // Snack-3
+    // La funzione "averange" calcola la media artimetica di un array di numeri.
+
+    test('La funzione "averange" calcola la media artimetica di un array di numeri', () => { 
+        expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
+        expect(average([10, 20, 30, 40, 50])).toBe(30);
+    })
 
    // Snack-7
    // La funzione findPostById restituisce il post corretto dato l'array di post e l'id
@@ -51,3 +57,6 @@ test('La funzione getInitials restituisce le iniziali di un nome completo.', () 
       expect(findPostById(posts, 4)).toEqual(null);
       expect(() => findPostById([36, 32], 4)).toThrow("L'Array Fornito Non è Corretto.");
     })
+})
+
+
